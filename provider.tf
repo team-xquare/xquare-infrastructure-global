b1aabbce24b1f4d7d573137485d5c48b5c2f4688
poka <khs0826@dsm.hs.kr>
2023-07-04 08:55:06 +0900

    ⚙️ :: Terraform Cloud 설정

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.46"
+    }
+  }
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "xquare"
+    workspaces {
+      name = "xquare-global"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
