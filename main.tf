a58a0667da3c85c106c7f8570d3e8464ae1aab63
rlaisqls <rlaisqls@gmail.com>
2023-07-09 23:50:36 +0900

    ⚙️ :: 초기화

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ecr" {
-  source = "github.com/team-xquare/xquare-infrastructure-module.git//modules/ecr?ref=v0.0.2"
-  name   = "test"
-  prod_image_limit = 3
-  prod_tag_prefix = "-prod"
-  stag_image_limit = 3
-  stag_tag_prefix = "-stag"
-}
