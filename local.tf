e5820c4c895a0db3180d3693a7db10bc657e49c9
poka <khs0826@dsm.hs.kr>
2023-07-21 14:26:50 +0900

    ⚡️:: create prod ecr

--- local.tf
+++ local.tf
@@ -17,9 +17,29 @@ locals {
     "report-be-stag",
     "schedule-be-stag",
     "timetable-be-stag",
-    "user-be-stag"]
+    "user-be-stag",
+  ]
   stag_tag_prefix = "stag-"
   stag_tag_limit  = 5
+
+  prod_ecr_names  = [
+    "application-be-prod",
+    "attachment-be-prod",
+    "authority-be-prod",
+    "backoffice-be-prod",
+    "contour-middleware-be-prod",
+    "feed-be-prod",
+    "meal-be-prod",
+    "notification-be-prod",
+    "pick-be-prod",
+    "point-be-prod",
+    "report-be-prod",
+    "schedule-be-prod",
+    "timetable-be-prod",
+    "user-be-prod",
+  ]
+  prod_tag_prefix = "v"
+  prod_tag_limit  = 5
 }
 
 locals {
