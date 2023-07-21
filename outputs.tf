db870ad846d4340222dff7533eaad8c3e2c1d682
poka <khs0826@dsm.hs.kr>
2023-07-21 14:26:50 +0900

    ⚡️:: create prod ecr

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "cluster_id" {
   value = module.eks.cluster_id
 }
+
+output "stag_ecr_url" {
+  value = module.stag_ecr.*.ecr_repository_url
+}
+
+output "prod_ecr_url" {
+  value = module.prod_ecr.*.ecr_repository_url
+}
