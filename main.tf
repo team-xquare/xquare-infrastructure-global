4309877f0485982b7c62a3a23f9bdafad8e62c80
poka <khs0826@dsm.hs.kr>
2023-07-26 18:12:58 +0900

    🔀 :: 모듈 변경 및 로그 그룹 삭제

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "stag_ecr" {
-  source      = "github.com/team-xquare/xquare-infrastructure-module.git//modules/ecr?ref=v0.0.4"
+  source      = "github.com/team-xquare/xquare-infrastructure-module.git//ecr?ref=v0.0.7"
 
   for_each    = local.stag_ecr_names
   name        = each.value
@@ -9,7 +9,7 @@ module "stag_ecr" {
 }
 
 module "prod_ecr" {
-  source      = "github.com/team-xquare/xquare-infrastructure-module.git//modules/ecr?ref=v0.0.4"
+  source      = "github.com/team-xquare/xquare-infrastructure-module.git//ecr?ref=v0.0.7"
 
   for_each    = local.prod_ecr_names
   name        = each.value
@@ -19,7 +19,7 @@ module "prod_ecr" {
 }
 
 module "vpc" {
-  source          = "github.com/team-xquare/xquare-infrastructure-module.git//modules/vpc?ref=v0.0.5"
+  source          = "github.com/team-xquare/xquare-infrastructure-module.git//vpc?ref=v0.0.7"
 
   vpc_cidr        = local.vpc_cidr
   azs             = local.azs
@@ -29,7 +29,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source          = "github.com/team-xquare/xquare-infrastructure-module.git//modules/eks-cluster?ref=v0.0.6"
+  source          = "github.com/team-xquare/xquare-infrastructure-module.git//eks-cluster?ref=v0.0.7"
 
   name_prefix     = local.name_prefix
   cluster_version = local.cluster_version
@@ -38,4 +38,6 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnet_ids
   public_subnets  = module.vpc.public_subnet_ids
+
+  create_cloudwatch_logs_group = false
 }
