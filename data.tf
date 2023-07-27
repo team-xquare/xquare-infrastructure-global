19f01d682a1ba192ae4240f7c543759a82a5b9e2
poka <khs0826@dsm.hs.kr>
2023-07-27 22:17:31 +0900

    🔀 :: 버그 수정

--- data.tf
+++ data.tf
@@ -1,19 +1,2 @@
 data "aws_region" "current" {}
 
-data "aws_iam_policy_document" "thanos_s3_policy" {
-  version = "2012-10-17"
-  statement {
-    sid    = ""
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:PutObject",
-    ]
-    resources = [
-      "arn:aws:s3:::${local.thanos_storage_name_prefix}-bucket",
-      "arn:aws:s3:::${local.thanos_storage_name_prefix}-bucket/*",
-    ]
-  }
-}
