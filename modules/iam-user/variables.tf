e54abf8346b6116c7897cec099cfafac675b9f52
rlaisqls <rlaisqls@gmail.com>
2023-08-01 01:15:38 +0900

    ⚡️ :: iam 모듈 추가

---
+++ modules/iam-user/variables.tf
@@ -0,0 +1,22 @@
+variable "name" {
+  description = "The user's name"
+  type = string
+}
+
+variable "create_iam_user_login_profile" {
+  description = "Whether to create IAM user login profile"
+  type        = bool
+  default     = false
+}
+
+variable "create_iam_access_key" {
+  description = "Whether to create IAM access key"
+  type        = bool
+  default     = true
+}
+
+variable "policy_arns" {
+  description = "The list of ARNs of policies directly assigned to the IAM user"
+  type        = list(string)
+  default     = []
+}
