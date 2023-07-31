6ebdd651a72376addb4159b3f05adcff866b9fc2
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-07-31 20:54:45 +0900

    ♻️

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,8 +1,7 @@
 module "sqs" {
-  source = "terraform-aws-modules/sqs/aws"
-  name   = "${var.name_prefix}-queue.fifo"
-
-  fifo_queue = var.fifo_queue
+  source                      = "terraform-aws-modules/sqs/aws"
+  name                        = "${var.name_prefix}-queue.fifo"
+  fifo_queue                  = var.fifo_queue
   content_based_deduplication = var.content_based_deduplication
 }
 
