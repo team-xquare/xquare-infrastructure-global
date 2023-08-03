428d244790ea18a63a2858d0a14dbf92c9d2fe55
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-03 22:41:37 +0900

    🔀 :: sg, subnet 추가

--- main.tf
+++ main.tf
@@ -71,4 +71,6 @@ resource "aws_db_instance" "xquare-db" {
   instance_class    = local.db_type
   availability_zone = "${data.aws_region.current.name}c"
   username          = local.db_username
+  vpc_security_group_ids    = [local.db_security_group_id]
+  db_subnet_group_name      = local.db_subnet_group_name
 }
