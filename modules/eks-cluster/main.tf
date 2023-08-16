5a52a44b4ba6ce5ba3ec118acd98c8df4f03c404
poka <khs0826@dsm.hs.kr>
2023-08-16 13:08:40 +0900

    ♻️ :: 수정

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -13,7 +13,7 @@ module "eks" {
 
   enable_irsa = true
 
-  cluster_enabled_log_types = [""]
+  cluster_enabled_log_types = []
   create_cloudwatch_log_group = false
 
   eks_managed_node_groups = {
