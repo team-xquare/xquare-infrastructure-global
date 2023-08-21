6999d3713dbc31ff07af78fd17c4dfd194ce749f
poka <khs0826@dsm.hs.kr>
2023-08-21 14:57:54 +0900

    karpenter 추가

---
+++ modules/eks-cluster/modules/karpenter/main.tf
@@ -0,0 +1,46 @@
+module "karpenter" {
+  source                          = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version                         = "18.31.0"
+  cluster_name                    = var.cluster_name
+  irsa_oidc_provider_arn          = var.irsa_oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+  create_iam_role                 = false
+  iam_role_arn                    = var.iam_role_arn
+}
+
+resource "helm_release" "karpenter" {
+  namespace = "karpenter"
+  create_namespace = true
+
+  name  = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = var.repository_username
+  repository_password = var.repository_password
+  chart               = "karpenter"
+  version             = "v0.29.2"
+
+  set {
+    name  = "settings.aws.clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "settings.aws.clusterEndpoint"
+    value = var.cluster_endpoint
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter.irsa_arn
+  }
+
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = module.karpenter.instance_profile_name
+  }
+
+  set {
+    name  = "settings.aws.interruptionQueueName"
+    value = module.karpenter.queue_name
+  }
+}
