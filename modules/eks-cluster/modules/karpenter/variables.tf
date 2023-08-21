afe0c0b46c5eab156e5e5138190dbbdc903f527e
poka <khs0826@dsm.hs.kr>
2023-08-21 14:57:54 +0900

    karpenter 추가

---
+++ modules/eks-cluster/modules/karpenter/variables.tf
@@ -0,0 +1,12 @@
+variable "cluster_name" {
+}
+variable "irsa_oidc_provider_arn" {
+}
+variable "iam_role_arn" {
+}
+variable "cluster_endpoint" {
+}
+variable "repository_username" {
+}
+variable "repository_password" {
+}
