8c7a925bd57452b648bc3e605584a9e3af43307a
rlaisqls <rlaisqls@gmail.com>
2023-08-26 13:01:01 +0900

    ⚡️ :: helm 추가

--- modules/eks-cluster/modules/karpenter/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cluster_name" {
-}
-variable "irsa_oidc_provider_arn" {
-}
-variable "iam_role_arn" {
-}
-variable "cluster_endpoint" {
-}
---
+++ modules/helm/variables.tf
@@ -0,0 +1,24 @@
+variable "repository" {
+    type = string
+    default = "https://team-xquare.github.io/k8s-resource"
+}
+variable "name" {
+    type = string
+}
+variable "namespace" {
+    type = string
+}
+variable "chart" {
+    type = string
+}
+variable "chart_version" {
+    type = string
+}
+variable "cleanup_on_fail" {
+    type = bool
+    default = true
+} 
+variable "create_namespace" {
+    type = bool
+    default = true
+}
