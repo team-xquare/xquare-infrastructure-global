dbd6c1f7f9b58774ca99b7396011102ee0362add
rlaisqls <rlaisqls@gmail.com>
2023-09-02 14:36:48 +0900

    ⚡️ ::  local 이름 중복 해결

--- elasticcache.tf
+++ elasticcache.tf
@@ -1,9 +1,9 @@
 locals {
   elasticcache_name = "xquare-redis"
-  engine = "redis"
-  engin_version = "7.0"
-  node_type = "cache.t2.micro"
-  parameter_group_name = "default.redis7"
+  elasticcache_engine = "redis"
+  elasticcache_engin_version = "7.0"
+  elasticcache_node_type = "cache.t2.micro"
+  elasticcache_parameter_group_name = "default.redis7"
 }
 
 resource "aws_security_group" "elasticache_sg" {
@@ -27,16 +27,16 @@ resource "aws_security_group" "elasticache_sg" {
 
 resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
   name       = "${local.elasticcache_name}-cache-subnet"
-  subnet_ids = [ module.vpc.private_subnet_ids ]
+  subnet_ids = module.vpc.private_subnet_ids
 }
 
 resource "aws_elasticache_cluster" "xquare-cluster" {
   cluster_id           = local.elasticcache_name
-  engine               = local.engine
-  engine_version       = local.engin_version
-  node_type            = local.node_type
+  engine               = local.elasticcache_engine
+  engine_version       = local.elasticcache_engin_version
+  node_type            = local.elasticcache_node_type
   num_cache_nodes      = 1
-  parameter_group_name = local.parameter_group_name
+  parameter_group_name = local.elasticcache_parameter_group_name
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.elasticache_subnet_group
   security_group_ids = [ aws_security_group.elasticache_sg.vpc_id ]
