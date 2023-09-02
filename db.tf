a56a9db33fd3d80490342b19432b6a51ace770dc
rlaisqls <rlaisqls@gmail.com>
2023-09-02 16:18:07 +0900

    🔀 :: security group

--- db.tf
+++ db.tf
@@ -3,9 +3,21 @@ locals {
   db_engine            = "mysql"
   db_storage_size      = 20
   db_username          = "admin"
-  db_security_group_id = "sg-0a33caeeab6bd2153"
   db_subnet_group_name = "default-vpc-00dba85fbdc1b606e"
   db_public_accessible = true
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db-security-group"
+  description = "Security group for rds on port 3306"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_db_instance" "xquare-db" {
@@ -16,7 +28,7 @@ resource "aws_db_instance" "xquare-db" {
   availability_zone      = "${data.aws_region.current.name}c"
   username               = local.db_username
   password               = var.rds_master_password
-  vpc_security_group_ids = [local.db_security_group_id]
+  vpc_security_group_ids = [ aws_security_group.db_sg.id ]
   db_subnet_group_name   = local.db_subnet_group_name
   publicly_accessible    = local.db_public_accessible
 }
--- elasticcache.tf
+++ elasticcache.tf
@@ -11,6 +11,19 @@ resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
   subnet_ids = module.vpc.private_subnet_ids
 }
 
+resource "aws_security_group" "redis_sg" {
+  name        = "redis-security-group"
+  description = "Security group for Redis on port 6379"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
 resource "aws_elasticache_cluster" "xquare-cluster" {
   cluster_id           = local.elasticcache_name
   engine               = local.elasticcache_engine
@@ -20,5 +33,5 @@ resource "aws_elasticache_cluster" "xquare-cluster" {
   parameter_group_name = local.elasticcache_parameter_group_name
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.elasticache_subnet_group.name
-  security_group_ids = [ local.db_security_group_id ]
+  security_group_ids = [ aws_security_group.redis_sg.id ]
 }
