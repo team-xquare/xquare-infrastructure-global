92c099410a2905e7e41136155a520cbb572c7104
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-02 11:09:58 +0900

    ♻️ :: node 유형 다르게 분리

--- modules/eks-cluster/local.tf
+++ modules/eks-cluster/local.tf
@@ -6,6 +6,7 @@ locals {
   public_subnets   = var.public_subnets
   private_subnets  = var.private_subnets
   current_username = element(split("/", data.aws_caller_identity.current.arn), 1)
-  instance_type    = var.instance_type
-  capacity_type    = var.capacity_type
+  instance_type    = ["m5a.large", "m5a.xlarge"]
+  capacity_type    = ["ON_DEMAND", "SPOT"]
+  role_policy      = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
 }
