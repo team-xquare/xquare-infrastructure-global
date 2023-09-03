4f2cc56eebd3703ac3ce811ee7f8e8bfca6d7800
poka <khs0826@dsm.hs.kr>
2023-09-04 07:53:13 +0900

    🔀 :: istio 버전 업그레이드

--- helm.tf
+++ helm.tf
@@ -28,7 +28,7 @@ locals {
   dex-k8s-authenticator-version = "1.4.2"
 
   istio-name    = "istio"
-  istio-version = "1.0.19"
+  istio-version = "1.0.20"
 
   karpenter-name    = "karpenter"
   karpenter-version = "1.0.3"
