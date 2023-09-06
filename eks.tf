cde8616cc2de097cbe894be181b78a99582de2d7
rlaisqls <rlaisqls@gmail.com>
2023-09-06 12:19:24 +0900

    ⚡️ :: 기존 cluster 제거

--- eks.tf
+++ eks.tf
@@ -4,27 +4,6 @@ locals {
   capacity_type   = "SPOT"
 }
 
-module "eks" {
-  source = "./modules/eks-cluster"
-
-  name_prefix     = local.name_prefix
-  cluster_version = local.cluster_version
-  instance_type   = local.node_type
-  capacity_type   = local.capacity_type
-
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnet_ids
-  public_subnets  = module.vpc.public_subnet_ids
-
-  nodegroup_min_size     = 3
-  nodegroup_max_size     = 6
-  nodegroup_desired_size = 5
-}
-
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
-
 module "eksv2" {
   source                 = "./modules/eks-cluster-v2"
 
@@ -39,4 +18,9 @@ module "eksv2" {
   nodegroup_min_size     = 3
   nodegroup_max_size     = 6
   nodegroup_desired_size = 5
+}
+
+
+output "cluster_id" {
+  value = module.eksv2.cluster_id
 }
