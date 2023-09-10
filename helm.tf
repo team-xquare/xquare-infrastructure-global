9518228fd1e63dd99dbf04d57256a5de00e89b4c
poka <khs0826@dsm.hs.kr>
2023-09-10 23:52:40 +0900

    🔀 :: karpenter version 업

--- helm.tf
+++ helm.tf
@@ -32,7 +32,7 @@ locals {
   istio-version = "1.0.26"
 
   karpenter-name    = "karpenter"
-  karpenter-version = "1.0.14"
+  karpenter-version = "1.0.15"
 
   kube-oidc-proxy-name    = "kube-oidc-proxy"
   kube-oidc-proxy-version = "0.3.3"
