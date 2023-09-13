45dcd8327ca48780000280969d93866e7fb4267e
rlaisqls <rlaisqls@gmail.com>
2023-09-13 20:05:53 +0900

    🔀 :: loki s3

--- s3.tf
+++ s3.tf
@@ -2,6 +2,7 @@ locals {
   prod_storage_name   = "xquare-prod-bucket"
   stag_storage_name   = "xquare-stag-bucket"
   thanos_storage_name = "xquare-thanos-bucket"
+  loki_storage_name = "xquare-loki-bucket"
 }
 
 module "prod_storage" {
@@ -22,5 +23,12 @@ module "thanos_storage" {
   source = "./modules/s3"
 
   bucket_name = local.thanos_storage_name
+  is_public   = false
+}
+
+module "loki_storage" {
+  source = "./modules/s3"
+
+  bucket_name = local.loki_storage_name
   is_public   = false
 }
