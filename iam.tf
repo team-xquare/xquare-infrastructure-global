2d8678ae34b54e199d81414e0d33b3284242963f
김은빈 <rlaisqls@gmail.com>
2023-09-13 13:35:32 +0900

    🔀 :: 오타

--- iam.tf
+++ iam.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "s3_policy_document" {
     actions = ["s3:*"]
     resources = [
       module.prod_storage.arn,
-      "${module.prod_storage.arn,}/*"
+      "${module.prod_storage.arn}/*"
       module.stag_storage.arn,
       "${module.stag_storage.arn}/*"
     ]
