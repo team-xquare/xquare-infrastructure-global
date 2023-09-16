71378198ced2ab3c0bd58b6557c8089e82f3225f
rlaisqls <rlaisqls@gmail.com>
2023-09-16 09:51:52 +0900

    ⚡️ :: node group 삭제

--- modules/eks-cluster-v2/main.tf
+++ modules/eks-cluster-v2/main.tf
@@ -16,31 +16,6 @@ module "eks" {
   cluster_enabled_log_types = []
   create_cloudwatch_log_group = false
 
-  eks_managed_node_group_defaults = {
-    instance_types = [local.instance_type]
-    capacity_type  = local.capacity_type
-  }
-  eks_managed_node_groups = {
-    initial = {
-      instance_types         = [local.instance_type]
-      create_security_group  = false
-      create_launch_template = false
-      launch_template_name   = ""
-
-      min_size     = var.nodegroup_min_size
-      max_size     = var.nodegroup_max_size
-      desired_size = var.nodegroup_desired_size
-
-      iam_role_additional_policies = [
-        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      ]
-      extend_config = {
-        bootstrap_extra_args = var.bootstrap_extra_args
-        pre_bootstrap_user_data = var.pre_bootstrap_user_data
-      }
-    }
-  }
-
   node_security_group_additional_rules = {
     ingress_nodes_karpenter_port = {
       description                   = "Cluster API to Node group for Karpenter webhook"
@@ -58,13 +33,14 @@ module "eks" {
 
   manage_aws_auth_configmap = true
 
-  aws_auth_users = [
-    {
+  aws_auth_users = concat(
+    [{
       userarn  = data.aws_caller_identity.current.arn
       username = local.current_username
       groups   = ["system:masters"]
-    }
-  ]
+    }],
+    var.additional_auth_users
+  )
 
   aws_auth_accounts = [
     data.aws_caller_identity.current.account_id
--- modules/eks-cluster-v2/variables.tf
+++ modules/eks-cluster-v2/variables.tf
@@ -6,19 +6,11 @@ variable "vpc_id" {
 }
 variable "public_subnets" {
 }
-variable "instance_type" {
+variable "additional_auth_users" {
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
 }
-variable "capacity_type" {
-}
-
-variable "nodegroup_min_size" {
-}
-variable "nodegroup_max_size" {
-}
-variable "nodegroup_desired_size" {
-}
-variable "bootstrap_extra_args" {
-}
-variable "pre_bootstrap_user_data" {
-}
-
