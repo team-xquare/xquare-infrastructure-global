0d936bd34c08db9b6ba18dd54e00510d6bdc76f9
poka <khs0826@dsm.hs.kr>
2023-09-17 23:22:22 +0900

    🔀 :: karpenter version up

--- helm.tf
+++ helm.tf
@@ -22,7 +22,7 @@ locals {
   istio-version = "1.0.30"
 
   karpenter-name    = "karpenter"
-  karpenter-version = "1.0.30"
+  karpenter-version = "1.0.31"
 
   xquare-application-name    = "xquare-application"
   xquare-application-version = "1.0.16"
