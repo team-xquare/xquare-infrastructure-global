d7bd10091fab83e25f6b49cde97c52826206e58d
poka <khs0826@dsm.hs.kr>
2023-09-18 19:10:18 +0900

    🔀 :: istio version up

--- helm.tf
+++ helm.tf
@@ -19,7 +19,7 @@ locals {
   cert-manager-version = "v1.12.12"
 
   istio-name    = "istio"
-  istio-version = "1.0.33"
+  istio-version = "1.0.34"
 
   karpenter-name    = "karpenter"
   karpenter-version = "1.0.35"
