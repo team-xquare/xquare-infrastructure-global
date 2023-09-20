3e059e4c74b3fc1fe1ae97b9fdbabcd64b974bf7
poka <khs0826@dsm.hs.kr>
2023-09-20 20:01:05 +0900

    🔀 :: xquare-application version up

--- helm.tf
+++ helm.tf
@@ -25,7 +25,7 @@ locals {
   karpenter-version = "1.0.35"
 
   xquare-application-name    = "xquare-application"
-  xquare-application-version = "1.0.16"
+  xquare-application-version = "1.0.17"
  
   argocd-namespace       = "argocd"
   dex-namespace          = "dex"
