ecf91b9983c4c952c150d84962f55f8c6e84c934
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-08 21:51:51 +0900

    add :: cloudflare 관련 variables 추가

--- variables.tf
+++ variables.tf
@@ -1,4 +1,47 @@
 variable "rds_master_password" {
-  type    = string
-  default = "password"
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare_email" {
+  type = string
+}
+
+variable "xquare_cloudflare_zone_id" {
+  type = string
+}
+
+variable "xquare_cname_value" {
+  type = string
+}
+
+variable "xquare_design" {
+  type = string
+}
+
+variable "xquare_mysql" {
+  type = string
+}
+
+variable "xquare_redis" {
+  type = string
+}
+
+variable "pick_cloudflare_zone_id" {
+  type = string
+}
+
+variable "repo_cloudflare_zone_id" {
+  type = string
+}
+
+variable "repo_a_value" {
+  type = string
+}
+
+variable "repo_resume" {
+  type = string
 }
