ed9975f7fac21aeeb1c2929494065343de1003ba
김은빈 <rlaisqls@gmail.com>
2023-11-29 19:20:04 +0900

    🔀 :: variable default 값 추가

--- variables.tf
+++ variables.tf
@@ -1,37 +1,46 @@
 variable "rds_master_password" {
   type = string
+  default = ""
 }
 
 variable "cloudflare_api_token" {
   type = string
+  default = ""
 }
 
 variable "xquare_cloudflare_zone_id" {
   type = string
+  default = ""
 }
 
 variable "pick_cloudflare_zone_id" {
   type = string
+  default = ""
 }
 
 variable "repo_cloudflare_zone_id" {
   type = string
+  default = ""
 }
 
 variable "xquare_server_domain" {
   type = string
+  default = ""
 }
 
 variable "xquare_design_domain" {
   type = string
+  default = ""
 }
 
 variable "xquare_mysql_domain" {
   type = string
+  default = ""
 }
 
 variable "xquare_redis_domain" {
   type = string
+  default = ""
 }
 
 variable "repo_on_premise_ip" {
