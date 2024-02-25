c4c166be0dcb959b988959ad16f4ecfe4e3a223c
HyunSu1768 <azxcv1768@gmail.com>
2024-02-26 06:05:38 +0900

    feat :: version up

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = "~> 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.46"
+      version = "~> 5.38"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
