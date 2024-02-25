3b7251d1e4901de6f537bc58a05eb92f95edf8d5
HyunSu1768 <azxcv1768@gmail.com>
2024-02-26 08:26:11 +0900

    feat :: 제발

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -29,7 +29,7 @@ resource "helm_release" "karpenter" {
 
   set {
     name  = "karpenter.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "arn:aws:iam::471407337433:role/KarpenterControllerRole-xquare-v2-cluster"
+    value = module.karpenter.irsa_arn
   }
 
   set {
