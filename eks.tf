15d30db327a26447d4e8ff7b48dafabd571f074b
HyunSu1768 <azxcv1768@gmail.com>
2024-03-01 19:08:39 +0900

    fix :: 노드개수 1

--- eks.tf
+++ eks.tf
@@ -18,7 +18,7 @@ module "eksv2" {
   public_subnets  = module.vpc.public_subnet_ids
   nodegroup_min_size     = 1
   nodegroup_max_size     = 3
-  nodegroup_desired_size = 2
+  nodegroup_desired_size = 1
 
   bootstrap_extra_args = "--use-max-pods false --kubelet-extra-args '--max-pods=110'"
   pre_bootstrap_user_data = <<-EOT
