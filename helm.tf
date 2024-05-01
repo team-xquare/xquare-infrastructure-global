4bda419c2161484542a9e3fd074c3efb81509f3a
HyunSu1768 <azxcv1768@gmail.com>
2024-05-01 12:26:22 +0900

    feat :: xquare-application-version up

--- helm.tf
+++ helm.tf
@@ -25,7 +25,7 @@ locals {
   karpenter-version = "1.0.61"
 
   xquare-application-name    = "xquare-application"
-  xquare-application-version = "1.0.46"
+  xquare-application-version = "1.0.47"
  
   argocd-namespace       = "argocd"
   dex-namespace          = "dex"
