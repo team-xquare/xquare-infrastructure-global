1254abcbfb053f2a67b1d38c8d691a39305c02c2
HyunSu1768 <azxcv1768@gmail.com>
2024-05-25 18:50:04 +0900

    feat :: event bridge rule 추가

---
+++ modules/lambda/outputs.tf
@@ -0,0 +1,7 @@
+output "this_lambda_function_arn" {
+  value = module.lambda_function.lambda_function_arn
+}
+
+output "this_lambda_function_name" {
+  value = module.lambda_function.lambda_function_name
+}
