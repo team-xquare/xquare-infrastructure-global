98b5e076e4954676275286bde6e9412c8fdb58e5
HyunSu1768 <azxcv1768@gmail.com>
2024-07-09 07:21:59 +0900

    refactor :: private subnet 삭제

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -14,9 +14,9 @@ variable "public_subnets" {
   type    = list(string)
 }
 
-variable "private_subnets" {
-  type    = list(string)
-}
+# variable "private_subnets" {
+#   type    = list(string)
+# }
 
 variable "cluster_name" {
   type = string
