45d13976b13aeb48a2784a4a738aeac6bac56311
HyunSu1768 <azxcv1768@gmail.com>
2024-07-09 08:10:38 +0900

    feat :: rollback

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
 
   vpc_cidr        = local.vpc_cidr
   azs             = local.azs
-#   private_subnets = local.private_subnets
+  private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
   public_subnet_tags_per_az = {
     "${local.region}a" = {
