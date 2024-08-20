01ef9c32a792808fb064450cd46e3cb247a951eb
HyunSu1768 <azxcv1768@gmail.com>
2024-08-20 12:23:05 +0900

    Update spot price for EC2 instance request

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "example" {
   ami           = "ami-0af209700f3504393"
   instance_type = "t3a.large"  # 2 vCPU, 7 GiB Memory
 
-  spot_price    = "0.0257"
+  spot_price    = "0.0323"
   spot_type     = "one-time"
   wait_for_fulfillment = true
 
