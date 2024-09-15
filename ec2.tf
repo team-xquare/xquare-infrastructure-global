0da09c1d5f14db3493e9113029632639e8702519
현수 <azxcv1768@gmail.com>
2024-09-15 14:10:26 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "example" {
-  ami           = "ami-0cf6568d72c50d9d0"
+  ami           = "	ami-0e630e42470754cb4"
   instance_type = "t3a.large"  # 2 vCPU, 7 GiB Memory
 
   spot_price    = "0.037"
