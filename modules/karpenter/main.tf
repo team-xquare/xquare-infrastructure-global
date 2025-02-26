7929f2cc66c7287d975975e28f620b5170dd5ff5
HyunSu1768 <azxcv1768@gmail.com>
2025-02-26 19:27:41 +0900

    karpenter module version up ;

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -1,13 +1,25 @@
 module "karpenter" {
   source                          = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version                         = "18.31.0"
+  version                         = "20.33.1"
   cluster_name                    = var.cluster_name
   irsa_oidc_provider_arn          = var.irsa_oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
-  create_iam_role                 = false
-  iam_role_arn                    = var.iam_role_arn
+
+  create_iam_role      = false
+
+  node_iam_role_arn    = var.iam_role_arn
+
+  enable_irsa             = true
+  create_instance_profile = true
+  iam_role_use_name_prefix = false
+  
+  iam_role_name          = "KarpenterIRSA-${var.cluster_name}"
+  iam_role_description   = "Karpenter IAM role for service account"
+  iam_policy_name        = "KarpenterIRSA-${var.cluster_name}"
+  iam_policy_description = "Karpenter IAM policy for service account"
 }
- 
+
+
 resource "helm_release" "karpenter" {
   namespace        = var.namespace
   create_namespace = true
@@ -18,27 +30,17 @@ resource "helm_release" "karpenter" {
   version    = var.chart_version
 
   set {
-    name  = "karpenter.setings.clusterName"
+    name  = "karpenter.settings.clusterName"
     value = var.cluster_name
   }
 
   set {
-    name  = "karpenter.setings.interruptionQueue"
+    name  = "karpenter.settings.interruptionQueue"
     value = var.cluster_name
   }
 
   set {
     name  = "karpenter.settings.clusterEndpoint"
-    value = var.cluster_endpoint
-  }
-
-  set {
-    name  = "karpenter.clusterName"
-    value = var.cluster_name
-  }
-
-  set {
-    name  = "karpenter.clusterEndpoint"
     value = var.cluster_endpoint
   }
 
@@ -51,10 +53,4 @@ resource "helm_release" "karpenter" {
     name  = "karpenter.aws.defaultInstanceProfile"
     value = module.karpenter.instance_profile_name
   }
-
-  set {
-    name  = "karpenter.settings.aws.defaultInstanceProfile"
-    value = module.karpenter.instance_profile_name
-  }
-
 }
