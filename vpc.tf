58ecc72488db4915c67a190bfdba351471558006
HyunSu1768 <azxcv1768@gmail.com>
2025-03-05 23:20:15 +0900

    fix :: rollback

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 locals {
   azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}c"]
   public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
-  private_subnets = ["10.0.128.0/2", "10.0.144.0/20"]
+  private_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
   vpc_cidr        = "10.0.0.0/16"
 }
 
